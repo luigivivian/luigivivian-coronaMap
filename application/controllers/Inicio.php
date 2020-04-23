1c25e2d9e4e2559b30db7ba2faa3274d729b065a
luigivivian <luigivivian@hotmail.com>
2020-04-23 05:11:59 -0300

    set headers

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -8,6 +8,13 @@ class Inicio extends CI_Controller{
 	public function __construct()
 	{
 		parent::__construct();
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            die();
+        }
 		$this->load->model('m_paciente');
         $this->load->model('m_estado');
         $this->load->model('m_unidade');
